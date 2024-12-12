685420ac2635128896eb2a73f12db71d8b4df80a
AnthonyZitoTegoshi-Evosystems <131671775+AnthonyZitoTegoshi-Evosystems@users.noreply.github.com>
2024-12-12 16:46:49 -0300

    Update utils.dart

--- lib/src/utils.dart
+++ lib/src/utils.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:intl/intl.dart';
 
 /// Returns the default week days as strings (using intl).
 List<String> defaultWeekDays() =>
-    DateFormat.E().dateSymbols.WEEKDAYS.map((e) => e.substring(0, 3)).toList();
+    DateFormat.E(Locale(AppLocalizations.of(context).localeName)).dateSymbols.WEEKDAYS.map((e) => e.substring(0, 3)).toList();
