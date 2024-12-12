3c47d9e93f09bc8c2d78895201d9091105777349
AnthonyZitoTegoshi-Evosystems <131671775+AnthonyZitoTegoshi-Evosystems@users.noreply.github.com>
2024-12-12 17:10:45 -0300

    Update month_selector.dart

--- lib/src/widgets/bricks/month_selector.dart
+++ lib/src/widgets/bricks/month_selector.dart
@@ -44,7 +44,7 @@ class MonthSelectorAndDoubleIndicator extends StatelessWidget {
         ),
         Expanded(
           child: Text(
-            DateFormat.yMMM(AppLocalizations.of(context)!.localeName).format(currentMonth),
+            DateFormat.yMMM(AppLocalizations.of(context)?.localeName).format(currentMonth),
             textAlign: TextAlign.center,
             style: style,
           ),
@@ -53,7 +53,7 @@ class MonthSelectorAndDoubleIndicator extends StatelessWidget {
           const SizedBox(width: 16),
           Expanded(
             child: Text(
-              DateFormat.yMMM(AppLocalizations.of(context)!.localeName).format(nextMonth!),
+              DateFormat.yMMM(AppLocalizations.of(context)?.localeName).format(nextMonth!),
               textAlign: TextAlign.center,
               style: style,
             ),
